930ea61d183bfab12418ec7773590af719977fe9
joel thomas mathew <joelmathew857@gmail.com>
2023-04-25 10:03:47 +0530

    added another imgaes

--- src/component/Experts.jsx
+++ src/component/Experts.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Laptop from "../ass/imga/istockphoto.jpeg";
+import cafe from "../ass/imga/cafe-logo.png"
 
 export default function Experts() {
   return (
     <div className="max-w-1[1240px] mx-auto border border-black  grid grid-cols-3">
-      <div className="border border-black h-400 w-200 col-span-1">
+      <div className="col-span-1 flex">
         <img src={Laptop} />
-        <imd src={Laptop} />
+        <img src={cafe}/>
+        <img src={cafe}/>
       </div>
     </div>
   );
